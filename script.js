40546a9ba98f374e2bbddd3bf32daeffe151f8f7
Joao Victor <imashinzo10@gmail.com>
2022-08-02 20:01:47 -0300

    Requisito 12 e personalizacao concluidos

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 const board = document.getElementById('pixel-board');
 const linhas = document.getElementsByClassName('lines');
 const pixels = document.getElementsByClassName('pixel');
+const pallets = document.getElementsByClassName('color');
+
+window.onload = function changepallets() {
+  pallets[0].style.backgroundColor = 'black';
+  for (let i = 1; i < pallets.length; i += 1) {
+    pallets[i].style.backgroundColor = `#${Math.floor(
+      Math.random() * 16777000
+    ).toString(16)}`;
+  }
+};
 
 let gridSize = 5;
 
