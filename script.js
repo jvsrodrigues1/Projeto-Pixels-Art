4a1557fea1d8250e5fcebc564d71a06bac60bf84
Joao Victor <imashinzo10@gmail.com>
2022-08-02 20:25:24 -0300

    Requisito 7 finalizado

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const linhas = document.getElementsByClassName('lines');
 const pixels = document.getElementsByClassName('pixel');
 const pallets = document.getElementsByClassName('color');
 
+// Ao carregar gera cores aleatorias para a paleta de cores e fixa a cor preta como primeira opcao. Referencia = https://css-tricks.com/snippets/javascript/random-hex-color/ //
+
 window.onload = function changepallets() {
   pallets[0].style.backgroundColor = 'black';
   for (let i = 1; i < pallets.length; i += 1) {
@@ -11,6 +13,8 @@ window.onload = function changepallets() {
     ).toString(16)}`;
   }
 };
+
+// funcao makelines e makepixels responsavel por gerar o grid de pintura 5x5 //
 
 let gridSize = 5;
 
@@ -33,3 +37,21 @@ function makePixels() {
   }
 }
 makePixels();
+
+// Uso da session storage para guardar a cor escolhida e perder a classe anterior, funcao change color para escolher cor a ser guardada. //
+
+sessionStorage.setItem('cor', 'black');
+function selectColor(event) {
+  sessionStorage.setItem('cor', event.target.style.backgroundColor);
+  for (index in pallets) {
+    pallets[index].classList.remove('selected');
+  }
+  event.target.classList.add('selected');
+}
+function changeColor() {
+  pallets[0].className = 'color selected';
+  for (let i = 0; i < pallets.length; i += 1) {
+    pallets[i].addEventListener('click', selectColor);
+  }
+}
+changeColor();
